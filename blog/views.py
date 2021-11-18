0a446ed2e8800737f0ccf49a04ccd5019be1f141
luthfiyyahnisaa <luthfiyyahnisaa@gmail.com>
2021-11-18 23:01:41 +0700

    Menambah register dan database

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
     context = {}
 
+    post = Post.objects.all()
+    context["posts"] = post
+    
     return render(request, "home.html", context)
 
 def about_view(request):
     context = {}
+    context["Nama"] = "Ofi"
+    context["Asal"] = "Padang"
+    context["Tinggi"] = "158"
+    context["BB"] = "57"
 
     return render(request, "about.html", context)
